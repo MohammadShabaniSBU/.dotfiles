47428cbf08e684de3a3ae4fa4ef63dfa22f38d1c
mohammad <mohamad.darghah@gmail.com>
2022-09-13 20:14:27 +0430

    add comment plugin. \n change colorscheme plugin

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,15 +1,45 @@
--- COLORSCHEMES
--- Uncomment just ONE of the following colorschemes!
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-dracula')
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-gruvbox-dark-medium')
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-monokai')
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-nord')
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-oceanicnext')
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-onedark')
--- local ok, _ = pcall(vim.cmd, 'colorscheme palenight')
---local ok, _ = pcall(vim.cmd, 'colorscheme base16-solarized-dark')
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-solarized-light')
--- local ok, _ = pcall(vim.cmd, 'colorscheme base16-tomorrow-night')
+-- config could be done for each theme
+-- Default options
+require('nightfox').setup({
+  options = {
+    -- Compiled file's destination location
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_file_suffix = "_compiled", -- Compiled file suffix
+    transparent = false,    -- Disable setting background
+    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = false,   -- Non focused panes set to alternative background
+    styles = {              -- Style to be applied to different syntax groups
+      comments = "NONE",    -- Value is any valid attr-list value `:help attr-list`
+      conditionals = "NONE",
+      constants = "NONE",
+      functions = "NONE",
+      keywords = "NONE",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "NONE",
+      variables = "NONE",
+    },
+    inverse = {             -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  },
+  palettes = {},
+  specs = {},
+  groups = {},
+})
 
---vim.g.base16_dracula_transparent_sidebar = true
---vim.g.base16_dracula_transparent= true
+-- setup must be called before loading
+-- uncomment theme to be set
+-- vim.cmd("colorscheme nightfox")
+-- vim.cmd("colorscheme dayfox")
+-- vim.cmd("colorscheme dawnfox")
+-- vim.cmd("colorscheme duskfox")
+-- vim.cmd("colorscheme nordfox")
+-- vim.cmd("colorscheme terafox")
+vim.cmd("colorscheme carbonfox")
