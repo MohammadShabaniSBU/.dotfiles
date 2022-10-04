83ab40f78acf1715d9a7870b9eaeeef468e3bc54
Mohammad Shabani <mohamad.darghah@gmail.com>
2022-10-04 20:19:15 +0330

    move lsp remapings to remap.lua

--- .config/nvim/lua/mohammad/remap.lua
+++ .config/nvim/lua/mohammad/remap.lua
@@ -2,34 +2,63 @@ local nnoremap = require("mohammad.keymap").nnoremap
 local inoremap = require("mohammad.keymap").inoremap
 local tnoremap = require("mohammad.keymap").tnoremap
 
-inoremap("jk", "<Esc>", {desc = "exit to normal mode"})
-inoremap("kj", "<Esc>", {desc = "exit to normal mode"})
+inoremap("jk", "<Esc>", { desc = "exit to normal mode" })
+inoremap("kj", "<Esc>", { desc = "exit to normal mode" })
 
 -- telescope keybindings
-inoremap("<C-p>", "<cmd>Telescope find_files<CR>", {desc = "telescope fuzzy finder"})
-nnoremap("<C-p>", "<cmd>Telescope find_files<CR>", {desc = "telescope fuzzy finder"})
-nnoremap("<leader>pv", "<cmd>Telescope file_browser<CR><Esc>", {desc = "toggle telescope file browser"})
-nnoremap("<leader>tc", "<cmd>Telescope colorscheme<CR><Esc>", {desc = "toggle telescope colorscheme picker"})
-nnoremap("<leader>ts", "<cmd>Telescope live_grep<CR><Esc>", {desc = "toggle telescope live grep (search string in project)"})
+inoremap("<C-p>", "<cmd>Telescope find_files<CR>", { desc = "telescope fuzzy finder" })
+nnoremap("<C-p>", "<cmd>Telescope find_files<CR>", { desc = "telescope fuzzy finder" })
+nnoremap("<leader>pv", "<cmd>Telescope file_browser<CR><Esc>", { desc = "toggle telescope file browser" })
+nnoremap("<leader>tc", "<cmd>Telescope colorscheme<CR><Esc>", { desc = "toggle telescope colorscheme picker" })
+nnoremap("<leader>ts", "<cmd>Telescope live_grep<CR><Esc>",
+    { desc = "toggle telescope live grep (search string in project)" })
 
 -- folding remaps
-nnoremap("z{", "zfa{", {desc = "fold {} block"})
-nnoremap("z(", "zfa(", {desc = "fold () block"})
-nnoremap("z[", "zfa[", {desc = "fold [] block"})
+nnoremap("z{", "zfa{", { desc = "fold {} block" })
+nnoremap("z(", "zfa(", { desc = "fold () block" })
+nnoremap("z[", "zfa[", { desc = "fold [] block" })
 
 -- split navigation
-nnoremap("<C-h>", "<C-w><C-h>", {desc = "go to the left screen"})
-nnoremap("<C-j>", "<C-w><C-j>", {desc = "go to the down screen"})
-nnoremap("<C-k>", "<C-w><C-k>", {desc = "go to the up screen"})
-nnoremap("<C-l>", "<C-w><C-l>", {desc = "go to the right screen"})
+nnoremap("<C-h>", "<C-w><C-h>", { desc = "go to the left screen" })
+nnoremap("<C-j>", "<C-w><C-j>", { desc = "go to the down screen" })
+nnoremap("<C-k>", "<C-w><C-k>", { desc = "go to the up screen" })
+nnoremap("<C-l>", "<C-w><C-l>", { desc = "go to the right screen" })
 
 -- floating terminal
-nnoremap("<leader><CR>", "<cmd>lua require('FTerm').toggle()<CR>", {desc = "toggle floating terminal"})
-tnoremap("<leader><CR>", "<cmd>lua require('FTerm').toggle()<CR>", {desc = "toggle floating terminal"})
+nnoremap("<leader><CR>", "<cmd>lua require('FTerm').toggle()<CR>", { desc = "toggle floating terminal" })
+tnoremap("<leader><CR>", "<cmd>lua require('FTerm').toggle()<CR>", { desc = "toggle floating terminal" })
+
+-- gitui interface
+nnoremap("<leader>gg", function() require("FTerm"):new({cmd= "gitui"}):toggle() end)
+
 
 -- harpoon keybindings
-nnoremap("<leader>ha", function() require("harpoon.mark").add_file() end, {desc = "add file to harpoon list"})
-nnoremap("<leader>ht", function() require("harpoon.ui").toggle_quick_menu() end, {desc = "toggle harpoon menu"})
-nnoremap("<leader>j", function() require("harpoon.ui").nav_file(1) end, {desc = "go to the first file of harpoon"})
-nnoremap("<leader>k", function() require("harpoon.ui").nav_file(2) end, {desc = "go to the second file of harpoon"})
-nnoremap("<leader>l", function() require("harpoon.ui").nav_file(3) end, {desc = "go to the third file of harpoon"})
+nnoremap("<leader>ha", function() require("harpoon.mark").add_file() end, { desc = "add file to harpoon list" })
+nnoremap("<leader>ht", function() require("harpoon.ui").toggle_quick_menu() end, { desc = "toggle harpoon menu" })
+nnoremap("<leader>j", function() require("harpoon.ui").nav_file(1) end, { desc = "go to the first file of harpoon" })
+nnoremap("<leader>k", function() require("harpoon.ui").nav_file(2) end, { desc = "go to the second file of harpoon" })
+nnoremap("<leader>l", function() require("harpoon.ui").nav_file(3) end, { desc = "go to the third file of harpoon" })
+
+-- lsp keybindings
+nnoremap('<leader>dl', vim.diagnostic.open_float, { desc = "show diagnostic of current line" })
+nnoremap('<leader>dw', vim.diagnostic.setloclist, { desc = "open diagnostic widnow of current buffer" })
+
+nnoremap('<leader>gd', vim.lsp.buf.definition, { desc = "go to definition" })
+nnoremap('<leader>gD', vim.lsp.buf.declaration, { desc = "go to declaration" })
+nnoremap('<leader>gt', vim.lsp.buf.type_definition, { desc = " go to type definition" })
+nnoremap('<leader>gi', vim.lsp.buf.implementation, { desc = "go to implementation" })
+
+nnoremap('<leader>k', vim.lsp.buf.hover, { desc = "show type definition (dialog)" })
+nnoremap('<leader>K', vim.lsp.buf.signature_help, { desc = "show signature help" })
+
+nnoremap('<leader>rn', vim.lsp.buf.rename, { desc = "rename the symbol name of the symbol under cursor" })
+nnoremap('<leader>la', vim.lsp.buf.code_action, { desc = "do code action for the line under cursor" })
+nnoremap('<leader>lr', vim.lsp.buf.references, { desc = "open references window for symbol under cursor" })
+
+nnoremap('<leader>wa', vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
+nnoremap('<leader>wr', vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
+nnoremap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, { desc = "list workspace folders" })
+
+nnoremap('<leader>f', vim.lsp.buf.formatting, { desc = "format the buffer" })
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,48 +1,11 @@
--- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+-- See `:help vim.lsp.*` for documentation on any of the below functions
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-end
-
-local lsp_flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
-}
-
-lspconfig = require "lspconfig"
-util = require "lspconfig/util"
+local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
 
 -- rust config
-lspconfig.rust_analyzer.setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    -- Server-specific settings...
+lspconfig.rust_analyzer.setup {
     settings = {
         ["rust-analyzer"] = {}
     }
@@ -50,9 +13,8 @@ lspconfig.rust_analyzer.setup{
 
 -- go config
 lspconfig.gopls.setup {
-    on_attach = on_attach,
-    cmd = {"gopls", "serve"},
-    filetypes = {"go", "gomod"},
+    cmd = { "gopls", "serve" },
+    filetypes = { "go", "gomod" },
     root_dir = util.root_pattern("go.work", "go.mod", ".git"),
     settings = {
         gopls = {
@@ -65,22 +27,16 @@ lspconfig.gopls.setup {
 }
 
 -- javascript / typescript config
-lspconfig.tsserver.setup{
-    on_attach = on_attach
-}
+lspconfig.tsserver.setup {}
 
-lspconfig.dockerls.setup{
-    on_attach = on_attach,
-}
+-- docker config
+lspconfig.dockerls.setup {}
 
-lspconfig.sumneko_lua.setup{
-    on_attach = on_attach,
-}
+-- lua config
+lspconfig.sumneko_lua.setup {}
 
-lspconfig.html.setup{
-    on_attach = on_attach,
-}
+-- html config
+lspconfig.html.setup {}
 
-lspconfig.tailwindcss.setup{
-    on_attach = on_attach,
-}
+-- tailwindcss config
+lspconfig.tailwindcss.setup {}
