af9618feb3a337f2ea7bfb1a970b701db8030a8d
Mohammad Shabani <mohamad.darghah@gmail.com>
2022-10-26 13:11:47 +0330

    make functions

--- installer.sh
+++ installer.sh
@@ -6,29 +6,31 @@ sudo pacman -S --noconfirm $(cat ./packages.txt)
 
 stow -t ~/ .
 
-if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-        ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
-else
-fi
-
-nvim --headless \
-    +so ~/.config/nvim/lua/mohammad/packer.lua \
-    +PackerInstall \
-    +q
-
-nvim --headless \
-    +so ~/.config/nvim/lua/plugins/mason.lua \
-    +MasonInstall \
-        clangd \
-        dockerfile-language-server \
-        goimports \
-        gopls \
-        json-lsp \
-        python-lsp-server \
-        rust-analyzer \
-        typescript-language-server \
-    +qa
+function installNvim() {
+    if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+        git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+            ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
+    else
+    fi
+    
+    nvim --headless \
+        +so ~/.config/nvim/lua/mohammad/packer.lua \
+        +PackerInstall \
+        +q
+    
+    nvim --headless \
+        +so ~/.config/nvim/lua/plugins/mason.lua \
+        +MasonInstall \
+            clangd \
+            dockerfile-language-server \
+            goimports \
+            gopls \
+            json-lsp \
+            python-lsp-server \
+            rust-analyzer \
+            typescript-language-server \
+        +qa
+}
 
 function zsh_full() {
     #install oh my zsh
@@ -48,3 +50,6 @@ function zsh_full() {
 
     chsh -s "$(which zsh)"
 }
+
+installNvim
+zsh_full
